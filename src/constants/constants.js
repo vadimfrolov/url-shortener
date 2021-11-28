98fe09d5cc20b5e6670a14d24ac523504403ff4b
Vadim Frolov <vadimf74@ya.ru>
2021-11-29 00:13:50 +0300

    constants correction

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,13 +1,13 @@
 const constants = {
-  alertClassName: 'alert',
-  alertTextMessage: 'url is not correct',
+    alertClassName: 'alert',
+    alertTextMessage: 'url is not correct',
 
-  validatedClassName: 'validated',
-  validatedTextMessage: 'url is correct',
+    validatedClassName: 'validated',
+    validatedTextMessage: 'url is correct',
 
-  errorMessage: 'something wrong, sorry',
+    baseURL: 'https://api-ssl.bitly.com/v4/shorten',
 
-  token: process.env.REACT_APP_BITLY_AUTHORIZATION_TOKEN,
-}
+    token: process.env.REACT_APP_BITLY_AUTHORIZATION_TOKEN,
+};
 
 export default constants;
