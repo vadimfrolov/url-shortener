152001011847c4d69f9070b7f5661f5d5d492141
Vadim Frolov <vadimf74@ya.ru>
2021-11-28 23:49:43 +0300

    created constants file

---
+++ src/constants/constants.js
@@ -0,0 +1,13 @@
+const constants = {
+  alertClassName: 'alert',
+  alertTextMessage: 'url is not correct',
+
+  validatedClassName: 'validated',
+  validatedTextMessage: 'url is correct',
+
+  errorMessage: 'something wrong, sorry',
+
+  token: process.env.REACT_APP_BITLY_AUTHORIZATION_TOKEN,
+}
+
+export default constants;
